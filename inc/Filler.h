5db666ddf8dab91510d516ee279a97a1e8ea0511
LegendreM <legendre.maxime.isn@gmail.com>
2016-11-24 12:37:40 +0100

    libs

--- inc/Filler.h
+++ inc/Filler.h
@@ -1,6 +1,24 @@
 #ifndef FILLER_H
 # define FILLER_H
 /*
+@name: t_coord
+@brief: struct wich contain coord
+@attr:
+	@name: x
+	@type: int
+	@brief: x coord
+@attr:
+	@name: y
+	@type: int
+	@brief: y coord
+*/
+typedef struct	s_coord
+{
+	int			x;
+	int			y;
+}				t_coord;
+
+/*
 @name: t_params
 @brief: struct returned by parser and passed as parameter to AI
 @attr:
@@ -15,11 +33,15 @@
 	@brief: piece to place on the game_board,
 	each line finish by '\0'
 	the last line is a null pointer
+@attr:
+	@name: coord
+	@brief: struct wich contain coord of game_piece
 */
 typedef struct	s_params
 {
 	char		**game_board;
-	char		**game_piece;	
+	char		**game_piece;
+	t_coord		coord;
 }				t_params;
 
 #endif
