c3f5b3253ad335eba5fc3b593e28ad00aec46ce9
Julien LE-MENER <jle-mene@e2r7p22.42.fr>
2016-11-30 11:20:44 +0100

    Change directory for gui functions

---
+++ src/gui/loop_hook.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   loop_hook.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/06 10:52:11 by mlegendr          #+#    #+#             */
+/*   Updated: 2014/12/08 16:59:52 by mlegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "filler.h"
+
+int			loop_hook(t_env *env)
+{
+	clear_image_8u(env->win_img, create_pixel_8u(0x2f, 0x2f, 0x2f, 0x00));
+	draw_game_rack(env->win_img, create_pixel_8u(0x9f, 0x9f, 0x9f, 0x00),
+					 20, 20);
+	draw_circle_in_roi(env->win_img, (t_roi){.x = 10, .y = 20, .width = 100, .height = 200}, create_pixel_8u(0xAf, 0x00, 0x5f, 0x00));
+	/* Parser */
+	/* AI */
+	return (0);
+}
---
+++ src/gui/start_gui.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlegendr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/02 15:41:22 by mlegendr          #+#    #+#             */
+/*   Updated: 2016/11/30 11:17:41 by jle-mene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "filler.h"
+
+static int	key_press(int keycode, t_env *env)
+{
+	(void)keycode;
+	(void)env;
+	if (keycode == 53)
+		exit(0);
+	return (0);
+}
+
+static int	key_release(int keycode, t_env *env)
+{
+	(void)keycode;
+	(void)env;
+	return (0);
+}
+
+int			start_gui(void)
+{
+	t_mlx_env		mlx;
+	t_env			env;
+
+	mlx.win_width = 1200;
+	mlx.win_height = 600;
+	if (!(mlx.ptr = mlx_init()))
+		return (1);
+	if (!(mlx.win = mlx_new_window(mlx.ptr,
+		mlx.win_width, mlx.win_height, "filler")))
+		return (1);
+	env.mlx = &mlx;
+	env.win_img = new_mlx_image_8u(
+					env.mlx->ptr, env.mlx->win_width, env.mlx->win_height);
+	mlx_hook(env.mlx->win, KEYPRESS, KEYPRESSMASK, key_press, &env);
+	mlx_hook(env.mlx->win, KEYRELEASE, KEYRELEASEMASK, key_release, &env);
+	mlx_hook(env.mlx->win, REDCROSS, REDCROSSMASK, red_cross, 0);
+	mlx_loop_hook(env.mlx->ptr, loop_hook, &env);
+	mlx_expose_hook(env.mlx->win, expose, &env);
+	mlx_loop(env.mlx->ptr);
+	return (0);
+}
