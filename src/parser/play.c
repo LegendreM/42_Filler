a16dafdadcfb1145364991329403c09aefedacf5
David MEJIA <dmejia@e2r8p21.42.fr>
2016-11-30 14:35:59 +0100

    function to play a piece

---
+++ src/parser/play.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+#include <unistd.h>
+
+void	play(int x, int y) //work
+{
+	char* x_str;
+	char* y_str;
+
+	if((x_str = ft_itoa(x)) && (y_str = ft_itoa(y)))
+	{
+		write(1, x_str, ft_strlen(x_str));
+		write(1, " ", 1);
+		write(1, y_str, ft_strlen(y_str));
+		write(1, "\n", 1);
+	}
+	return ;
+}
