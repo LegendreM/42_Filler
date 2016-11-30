443bd13c9628eb7614ddfeb1f4cf8b975c29a65c
Julien LE-MENER <jle-mene@e2r7p22.42.fr>
2016-11-30 09:57:21 +0100

    Update new filler.h

---
+++ inc/filler.h
@@ -0,0 +1,56 @@
+#ifndef FILLER_H
+# define FILLER_H
+
+# include <mlx.h>
+# include <stdlib.h>
+
+# include "./libft.h"
+# include "./mlxlibft.h"
+# include "./get_next_line.h"
+/*
+@name: t_params
+@brief: struct returned by parser and passed as parameter to AI
+@attr:
+	@name: game_board
+	@type: char**
+	@brief: map of the game,
+	each line finish by '\0'
+	the last line is a null pointer
+@attr:
+	@name: game_piece
+	@type: char**
+	@brief: piece to place on the game_board,
+	each line finish by '\0'
+	the last line is a null pointer
+@attr:
+	@name: coord
+	@brief: struct wich contain coord of game_piece
+*/
+
+typedef struct	s_params
+{
+	char		player;
+	int			count_line;
+	char		**game_board;
+	char		**game_piece;
+	t_coord		board_size;
+	t_coord		piece_size;
+	t_coord		coord;
+}				t_params;
+
+typedef struct	s_env
+{
+	t_mlx_env		*mlx;
+	t_mlx_image_8u	*win_img;
+}				t_env;
+
+int		loop_hook(t_env *env);
+int		expose(t_env *env);
+void	draw_game_rack(
+			t_mlx_image_8u *dst,
+			const t_pixel_8u pixel,
+			const int rack_width,
+			const int rack_height
+		);
+
+#endif
