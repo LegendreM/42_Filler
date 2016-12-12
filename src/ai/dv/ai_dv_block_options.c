54987c1b2fcfbdb9a7eda2ef6d6c7bf4ce21b8ba
David MEJIA <dmejia@e2r7p22.42.fr>
2016-12-12 20:28:25 +0100

    ma meilleur ia mais pas du tout a la norme et du code en double

--- src/ai/dv/ai_dv_block_options.c
+++ src/ai/dv/ai_dv_block_options.c
@@ -1,4 +1,56 @@
 #include "ai_dv.h"
+////////////////////////CODE DUPLIQUER DE L'IA ai_dv_go_around.c
+static t_coord	opp_dist(t_coord *check, t_coord pos, \
+							t_params *params, int *dist)
+{
+	int		tmp_dist;
+	t_coord to_play;
+
+	tmp_dist = 0;
+	check->x = 0;
+	while (check->x < params->board_size.x)
+	{
+		if (params->game_board[check->y][check->x] != '.')
+			if (params->game_board[check->y][check->x] != params->player[0] \
+				&& params->game_board[check->y][check->x] != params->player[1])
+			{
+				tmp_dist = get_dist(pos, *check);
+				if (*dist > tmp_dist)
+				{
+					to_play.x = pos.x;
+					to_play.y = pos.y;
+					*dist = tmp_dist;
+				}
+			}
+		++check->x;
+	}
+	++check->y;
+	return (to_play);
+}
+
+static int		go_around(t_coord *to_play, int pos_size, \
+						t_coord *pos, t_params *params)
+{
+	int		i;
+	int		dist;
+	t_coord	check;
+
+	t_coord_init(&check);
+	i = 0;
+	dist = 10000;
+	while (pos_size != 0)
+	{
+		check.y = 0;
+		while (check.y < params->board_size.y)
+		{
+			*to_play = opp_dist(&check, pos[i], params, &dist);
+		}
+		++i;
+		--pos_size;
+	}
+	return (1);
+}
+/////////////////////////END DUP CODE
 
 static int	block_opp(t_params *params, t_coord coord, t_coord check,
 				int *blocked, char opp_game_board[params->board_size.y][params->board_size.x])
@@ -128,7 +180,7 @@ int			ai_launch(t_params *params, t_coord *to_play)
 	}
 	else 
 	{
-		go_close(to_play, pos_size.me, me, params);
+		go_around(to_play, pos_size.me, me, params);
 	}
 	return (1);
 }
