5eee84f68a72ceb25ec9a19c419fce5df33ab86d
David MEJIA <dmejia@e2r7p22.42.fr>
2016-12-12 20:12:19 +0100

    dv ai opti (ai_dv_go_around) need : get_possible_positions, t_coord_init and get_dist

--- src/ai/dv/ai_dv_go_around.c
+++ src/ai/dv/ai_dv_go_around.c
@@ -1,6 +1,7 @@
 #include "ai_dv.h"
 
-t_coord		opp_dist(t_coord *check, t_coord pos, t_params *params, int *dist)
+static t_coord	opp_dist(t_coord *check, t_coord pos, \
+							t_params *params, int *dist)
 {
 	int		tmp_dist;
 	t_coord to_play;
@@ -27,7 +28,7 @@ t_coord		opp_dist(t_coord *check, t_coord pos, t_params *params, int *dist)
 	return (to_play);
 }
 
-int			go_around(t_coord *to_play, int pos_size, \
+static int		go_around(t_coord *to_play, int pos_size, \
 						t_coord *pos, t_params *params)
 {
 	int		i;
@@ -50,7 +51,7 @@ int			go_around(t_coord *to_play, int pos_size, \
 	return (1);
 }
 
-int			ai_launch(t_params *params, t_coord *to_play)
+int				ai_launch(t_params *params, t_coord *to_play)
 {
 	t_coord		me[params->board_size.x * params->board_size.y];
 	int			pos_size;
